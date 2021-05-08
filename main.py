2e4f9226333b67fed71552ddb790f4e26be7f6ab
jackred <jackred@tuta.io>
2021-05-08 20:52:27 +0100

    UPDATE read videos from folder output one csv

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from skimage.color import rgb2gray
 from skimage.transform import resize
 import os
 from PIL import Image
-from math import floor
 
 
-## need to clean code
-th_slot = 13
+# need to clean code
+th_slot = 20
 th_skill = 70
 th_value = 13
 th_rarity = 40
+
+threshold = {'rarity': th_rarity, 'skill_1': th_skill, 'value_1': th_value,
+             'skill_2': th_skill, 'value_2': th_value, 'slot_1': th_slot,
+             'slot_2': th_slot, 'slot_3': th_slot}
 
 shape = (720, 1280, 3)
 
@@ -25,9 +28,9 @@ rarity_y = range(3, 22)
 
 slot_y = range(23, 47)
 
-slot_1_x = range(154, 181) # 26
-slot_2_x = range(182, 209) # 27
-slot_3_x = range(209, 236) # 27
+slot_1_x = range(154, 181)  # 26
+slot_2_x = range(182, 209)  # 27
+slot_3_x = range(210, 237)  # 27
 
 skill_x = range(23, 174)
 skill_1_y = range(89, 108)
@@ -37,8 +40,9 @@ value_x = range(228, 236)
 value_1_y = range(116, 131)
 value_2_y = range(166, 181)
 
-index=['rarity', 'skill_1', 'value_1', 'skill_2',
-       'value_2', 'slot_1', 'slot_2', 'slot_3']
+index = ['rarity', 'skill_1', 'value_1', 'skill_2',
+         'value_2', 'slot_1', 'slot_2', 'slot_3']
+
 
 def get_sub_frame(frame, x, y):
     return frame[y][:, x]
@@ -84,6 +88,10 @@ def is_different(f1, f2):
     return ((f1 == f2).sum()/f1.size) < 0.80
 
 
+def calculateDistance(i1, i2):
+    return np.sum((i1-i2)**2)
+
+
 def process_image(image):
     image = image[:, :, ::-1]
     image = resize(image, shape)
@@ -95,17 +103,26 @@ def extract_frames(video):
     frame = process_image(frame)
     res = []
     if not_stop:
-        res.append(get_charm_sub_frame(frame))
+        tmp = get_charm_sub_frame(frame)
+        res.append(tmp)
+        fp = tmp
         while(not_stop):
             not_stop, frame = video.read()
             if not_stop:
                 frame = process_image(frame)
                 tmp = get_charm_sub_frame(frame)
-                if is_different(res[-1], tmp):
+                if calculateDistance(tmp, fp) > 30:
                     res.append(tmp)
+                    fp = tmp
     return res
 
 
+def same_infos(i1, i2):
+    return all([calculateDistance(i1[i], i2[i]) < threshold[i]
+                for i in i2])
+
+
+# to clean
 def read_frames(video, data):
     not_stop, frame = video.read()
     frame = process_image(frame)
@@ -129,7 +146,27 @@ def read_frames(video, data):
                         or not ((reading == res.iloc[-1]).all())):
                     res = res.append(reading, ignore_index=True)
     return res
-    
+
+
+def read_video(filename, data):
+    cap = cv2.VideoCapture(filename)
+    return read_frames(cap, data)
+
+
+def read_videos(folder_name):
+    data = load_data()
+    files = os.listdir(folder_name)
+    res = pd.DataFrame(columns=index)
+    for i in files:
+        if i[-4:] == '.mp4':
+            res = res.append(read_video(folder_name + '/' + i, data))
+    return res
+
+
+def read_videos_store_csv(folder_name):
+    charms = read_videos(folder_name)
+    save_csv(charms, folder_name + '/output.csv')
+
 
 def get_all_infos(frame):
     res = {}
@@ -149,12 +186,9 @@ def show(f, cmap=None):
     plt.show(block=False)
 
 
-def calculateDistance(i1, i2):
-    return np.sum((i1-i2)**2)
-
-
 def find_proba(element, list_name):
-    return np.array([calculateDistance(element, e) for e in list_name.values()])
+    return np.array([calculateDistance(element, e)
+                     for e in list_name.values()])
 
 
 def get_index(ele, threshold):
@@ -198,14 +232,22 @@ def read_skill_charm(elem, list_elem, list_elem_name):
 
 def read_infos_charm(infos, data):
     res = pd.Series(index=index, dtype=str)
-    res['rarity'] = read_rarity_charm(infos['rarity'], data['rarities'], data['rarities_name'])
-    res['skill_1'] = read_skill_charm(infos['skill_1'], data['skills'], data['skills_name'])
-    res['value_1'] = read_value_charm(infos['value_1'], data['values'], data['values_name'])
-    res['skill_2'] = read_skill_charm(infos['skill_2'], data['skills'], data['skills_name'])
-    res['value_2'] = read_value_charm(infos['value_2'], data['values'], data['values_name'])
-    res['slot_1'] = read_value_charm(infos['slot_1'], data['slots'], data['slots_name'])
-    res['slot_2'] = read_value_charm(infos['slot_2'], data['slots'], data['slots_name'])
-    res['slot_3'] = read_value_charm(infos['slot_3'], data['slots'], data['slots_name'])
+    res['rarity'] = read_rarity_charm(
+        infos['rarity'], data['rarities'], data['rarities_name'])
+    res['skill_1'] = read_skill_charm(
+        infos['skill_1'], data['skills'], data['skills_name'])
+    res['value_1'] = read_value_charm(
+        infos['value_1'], data['values'], data['values_name'])
+    res['skill_2'] = read_skill_charm(
+        infos['skill_2'], data['skills'], data['skills_name'])
+    res['value_2'] = read_value_charm(
+        infos['value_2'], data['values'], data['values_name'])
+    res['slot_1'] = read_value_charm(
+        infos['slot_1'], data['slots'], data['slots_name'])
+    res['slot_2'] = read_value_charm(
+        infos['slot_2'], data['slots'], data['slots_name'])
+    res['slot_3'] = read_value_charm(
+        infos['slot_3'], data['slots'], data['slots_name'])
     return res
 
 
@@ -220,7 +262,8 @@ def load_data():
     data['rarities_name'] = np.array(list(data['rarities']))
     data['slots_name'] = np.array(list(data['slots']))
     return data
-    
+
+
 # dict keep insertion order
 def load(folder, gray=True):
     list_elem = os.listdir(folder)
@@ -277,5 +320,6 @@ def save_csv(charms, name):
                      + charms['skill_2'] + ' ' + charms['value_2']).str.strip()
     charms['slots'] = (charms['slot_1'] + '-' + charms['slot_2']
                        + '-' + charms['slot_3'])
-    charms = charms[['skill_1', 'value_1', 'skill_2', 'value_2', 'mix', 'slots']]
+    charms = charms[['skill_1', 'value_1', 'skill_2',
+                     'value_2', 'mix', 'slots']]
     charms.to_csv(name, index=False)
